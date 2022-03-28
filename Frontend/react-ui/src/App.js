9845818f1bbc45585398e4438e507a1542997ee3
SyntaX21 <kevin-boecker@hotmail.de>
2022-03-28 11:14:33 +0200

    Register From Done

--- Frontend/react-ui/src/App.js
+++ Frontend/react-ui/src/App.js
@@ -9,19 +9,22 @@ import {
   Form,
   FormControl,
   Button,
+  Row,
+  Breadcrumb,
 } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 function App() {
   return (
     <div>
       <Navbar bg="light" expand={false}>
         <Container fluid>
-          <Navbar.Brand href="#">Name der Applikation</Navbar.Brand>
           <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Brand href="#">Name der Applikation</Navbar.Brand>
           <Navbar.Offcanvas
             id="offcanvasNavbar"
             aria-labelledby="offcanvasNavbarLabel"
-            placement="end"
+            placement="start"
           >
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id="offcanvasNavbarLabel">
@@ -62,46 +65,52 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="justify-content-center">
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Vorname</Form.Label>
-            <Form.Control type="text" placeholder="Vornamen eingeben" />
-            <Form.Text className="text-muted"></Form.Text>
-          </Form.Group>
+      <Form className="w-50 position-absolute top-50 start-50 translate-middle">
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Vorname</Form.Label>
+              <Form.Control type="text" placeholder="Vornamen eingeben" />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Nachname</Form.Label>
+              <Form.Control type="text" placeholder="Nachnamen eingeben" />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+          </Col>
+        </Row>
 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Nachname</Form.Label>
-            <Form.Control type="text" placeholder="Nachnamen eingeben" />
-            <Form.Text className="text-muted"></Form.Text>
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>E-Mail Adresse</Form.Label>
+          <Form.Control type="email" placeholder="E-Mail Adresse eingeben" />
+        </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>E-Mail Adresse</Form.Label>
-            <Form.Control type="email" placeholder="E-Mail Adresse eingeben" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Passwort</Form.Label>
+              <Form.Control type="password" placeholder="Passwort" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Passwort bestätigen</Form.Label>
+              <Form.Control type="password" placeholder="Passwort bestätigen" />
+            </Form.Group>
+          </Col>
+        </Row>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Passwort</Form.Label>
-            <Form.Control type="password" placeholder="Passwort" />
-          </Form.Group>
+        <Button href="#" variant="primary" type="submit">
+          Registrieren
+        </Button>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Passwort bestätigen" />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Registrieren
-          </Button>
-
-          <p>
-            <a>Login</a>
-          </p>
-        </Form>
-      </div>
+        <Breadcrumb>
+          <Breadcrumb.Item href="#">Login</Breadcrumb.Item>
+        </Breadcrumb>
+      </Form>
     </div>
   );
 }

9c46e93c8912e33d099da70330a54bf14b55a827
SyntaX21 <kevin-boecker@hotmail.de>
2022-05-08 16:23:45 +0200

    There was an attempt...

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,125 @@
-const express = require("express");
-const app = express();
+// groups, appointments, users
 
-app.get("/", (req, res) => {
-  console.log("here");
-  res.send("serverjs");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// initialize express app
+const app = express();
+app.set("view enginge", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json);
+
+//gib port
+const port = 3001;
+
+const appointments = [
+  {
+    id: 1,
+    name: "Appointment 1",
+    description: "My Description",
+    location: "Downunder",
+    date: new Date(3, 2, 1),
+    time: "10:00",
+    userId: 1,
+    groupId: undefined,
+  },
+];
+
+const users = [
+  {
+    id: 1,
+    first: "User 1",
+    last: "",
+    email: "",
+    password: "",
+    groups: [1],
+  },
+];
+
+const groups = [
+  {
+    id: 1,
+    name: "Group 1",
+    description: "My Description",
+    memberIDs: [1],
+  },
+];
+
+// create a new appointment
+app.post("/appointments", (req, res) => {
+  const newAppointment = req.body;
+  newAppointment.id = appointments.length + 1;
+  appointments.push(newAppointment);
+  res.json(newAppointment);
 });
 
-app.listen(3001, () => {
-  console.log("Server started on port 3001");
+// get all appointments
+app.get("/appointments", (req, res) => {
+  res.json(appointments);
+});
+
+// get appointment by id
+app.get("/appointments/:id", (req, res) => {
+  const appointmentId = req.params.id;
+  const appointment = appointments.find((appointment) => appointment.id === parseInt(appointmentId));
+  if (appointment) {
+    res.json(appointment);
+  } else {
+    res.status(404).json({
+      message: "Appointment not found",
+    });
+  }
+});
+
+// delete appointment by id
+app.delete("/appointments/:id", (req, res) => {
+  const appointmentId = req.params.id;
+  const appointmentIndex = appointments.findIndex((appointment) => appointment.id === parseInt(appointmentId));
+  if (appointmentIndex !== -1) {
+    appointments.splice(appointmentIndex, 1);
+    res.json({
+      message: "Appointment deleted",
+    });
+  } else {
+    res.status(404).json({
+      message: "Appointment not found",
+    });
+  }
+});
+
+// update appointment by id
+app.put("/appointments/:id", (req, res) => {
+  const appointmentId = req.params.id;
+  const appointmentIndex = appointments.findIndex((appointment) => appointment.id === parseInt(appointmentId));
+  if (appointmentIndex !== -1) {
+    const updatedAppointment = req.body;
+    appointments[appointmentIndex] = updatedAppointment;
+    res.json(updatedAppointment);
+  } else {
+    res.status(404).json({
+      message: "Appointment not found",
+    });
+  }
+});
+
+// create a new user
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  //res.json(newUser);
+  res.redirect("/");
+});
+
+// create a new group
+app.post("/groups", (req, res) => {
+  const newGroup = req.body;
+  newGroup.id = groups.length + 1;
+  groups.push(newGroup);
+  res.json(newGroup);
+});
+
+// start express app
+app.listen(port, () => {
+  console.log("Server started on port " + port);
 });
--- Frontend/react-ui/src/components/forms/SignupForm.jsx
+++ Frontend/react-ui/src/components/forms/SignupForm.jsx
@@ -1,26 +1,22 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import { Form, Button, Row, Col, Breadcrumb } from "react-bootstrap";
 import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import { lastDayOfYear } from "date-fns/esm";
 
 function SignupForm() {
-  let { postSlug } = useParams();
-
-  useEffect(() => {
-    // Fetch post using the postSlug
-  }, [postSlug]);
-
   return (
     <Form className="w-100 position-absolute top-50 start-50 translate-middle px-2">
       <Row>
         <Col>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group className="mb-3" controlId="formBasicText" name="first">
             <Form.Label>Vorname</Form.Label>
             <Form.Control type="text" placeholder="Vornamen eingeben" />
             <Form.Text className="text-muted"></Form.Text>
           </Form.Group>
         </Col>
         <Col>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group className="mb-3" controlId="formBasicText" name="last">
             <Form.Label>Nachname</Form.Label>
             <Form.Control type="text" placeholder="Nachnamen eingeben" />
             <Form.Text className="text-muted"></Form.Text>
@@ -28,7 +24,7 @@ function SignupForm() {
         </Col>
       </Row>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="formBasicEmail" name="email">
         <Form.Label>E-Mail Adresse</Form.Label>
         <Form.Control type="email" placeholder="E-Mail Adresse eingeben" />
       </Form.Group>
@@ -37,7 +33,7 @@ function SignupForm() {
         <Col>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Passwort</Form.Label>
-            <Form.Control type="password" placeholder="Passwort" />
+            <Form.Control type="password" placeholder="Passwort" name="password" />
           </Form.Group>
         </Col>
         <Col>
